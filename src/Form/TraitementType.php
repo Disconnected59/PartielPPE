56dbcbb8e48ca6aef02bc29b80a3b3bd6c55ef88
Disconnected59 <Disconnected59@users.noreply.github.com>
2018-11-29 14:19:14 +0100

    Traitement VFinal VF

--- src/Form/TraitementType.php
+++ src/Form/TraitementType.php
@@ -3,6 +3,7 @@ namespace App\Form;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -14,7 +15,6 @@ class TraitementType extends AbstractType
 	public function buildForm(FormBuilderInterface $builder, array $options)
 	{
 		$builder
-			->add('id')
 			->add('date_debut',DateType::class)
 			->add('duree')
 			->add('nom_patient',TextType::class)
